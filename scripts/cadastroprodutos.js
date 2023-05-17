5e7ff6453da5bf1213a8d3fe2d6fdd34998ddffe
JoaoOtavioM <otaviomatosxd3@gmail.com>
2023-05-17 11:23:44 -0300

    rest of correction of priceNumber

--- scripts/cadastroprodutos.js
+++ scripts/cadastroprodutos.js
@@ -1,7 +1,22 @@
-let validaPreco = document.getElementsByName('precoDoProduto')[0];
-validaPreco.addEventListener('input',(e)=>{
+let nome = document.querySelector('#nomeDoProduto');
+let ingredientes = document.querySelector('#ingredientesDoProduto');
+let preco = document.querySelector('#precoDoProduto');
+let tipo = document.querySelector('#tipoDeAlimento');
+let foto = document.querySelector('#fotoProduto');
+let form = document.querySelector('form');
+form.addEventListener("submit",(w)=>{
+    if(!nome.value||!ingredientes.value||!preco.value||!tipo.value || !foto.value){
+        w.preventDefault();
+        alert("Todos os campos devem estar preenchidos!");
+    }
+})
+
+
+preco.addEventListener('keyup',(e)=>{
     console.log(e);
-    if(e.key >=48 && e.key<=57){
-        alert("Di")
+    if (e.keyCode >= 48 && e.keyCode <= 57) {
+        preco.value += e.key
+    } else {
+        preco.value = ""
     }
 })
