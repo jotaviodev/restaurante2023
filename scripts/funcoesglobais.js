73bba19fad58b94ffd97711bad507d5168470977
JoaoOtavioM <otaviomatosxd3@gmail.com>
2023-05-26 11:20:03 -0300

    changed menu

--- scripts/funcoesglobais.js
+++ scripts/funcoesglobais.js
@@ -1,9 +1,8 @@
 function alteraCorMenu(option){
-    option.style.backgroundSize="10px";
-    option.style.background="#FF4D00";
+    option.style.backgroundColor="#ff6b01e2";
     option.style.borderRadius="2px";
 
 }
 function retornaCorMenu(option){
-    option.style.background="#4B0101"
+    option.style.backgroundColor=""
 }
