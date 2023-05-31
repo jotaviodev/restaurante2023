84e13d94189931ff694864cd7f80c37c98b26ec3
JoaoOtavioM <otaviomatosxd3@gmail.com>
2023-05-31 10:07:42 -0300

    insert imgs produto complete

--- adm/produtos/insertprodutos.php
+++ adm/produtos/insertprodutos.php
@@ -4,12 +4,33 @@ require("../connectdb.php");
     $ingredientesDoProduto = $_POST['ingredientesDoProduto'];
     $precoDoProduto = (double)str_replace(",",".",$_POST['precoDoProduto']);
     $tipoDoAlimento = $_POST['tipoDeAlimento'];
-    $fotoProduto = $_POST['fotoProduto'];
+    $fotoProduto = $_FILES['fotoProduto'];
 
-    $query = "INSERT INTO produtos VALUES (0,'$nomeDoProduto','$ingredientesDoProduto','$precoDoProduto','$tipoDoAlimento','$fotoProduto')";
+    global $conteudo;
+     $arquivo = $fotoProduto;
+        if($arquivo['error'] == 0) {
+            // Abre o arquivo em modo binário
+            $handle = fopen($arquivo['tmp_name'], 'rb');
 
+            // Converte o arquivo em um formato adequado para salvar no banco de dados
+            $conteudo = fread($handle, filesize($arquivo['tmp_name']));
+
+            // Fecha o arquivo
+            fclose($handle);
+        }
+        $data = [
+            "id_produto"=>0,
+            "nome_produto"=>$nomeDoProduto,
+            "ingredientes_produto"=>$ingredientesDoProduto,
+            "preco_produto"=>$precoDoProduto,
+            "tipo_produto"=>$tipoDoAlimento,
+            "foto_produto"=>$conteudo
+        ];
+    $query = "INSERT INTO produtos (id_produto,nome_produto,ingredientes_produto,preco_produto,tipo_produto,foto_produto)VALUES (:id_produto,:nome_produto,:ingredientes_produto,:preco_produto,:tipo_produto,:foto_produto)";
+        
 // Executa a query
-$inserir = $conn->query($query);
+$inserir = $conn->prepare($query);
+$inserir->execute($data);
 
 if ($inserir) {
 echo "Notícia inserida com sucesso!";
