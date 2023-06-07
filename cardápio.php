bd3be7a11670c02e9c5ab0d84a84c225d7e2e7bb
JoaoOtavioM <otaviomatosxd3@gmail.com>
2023-06-07 13:59:28 -0300

    do corrections of routes adm

--- cardápio.php
+++ cardápio.php
@@ -37,7 +37,7 @@
                     </div>
                     <li><a href="./index.html" onmouseover="alteraCorMenu(this)" onmouseout="retornaCorMenu(this)" id="teste">Início</a></li>
                     <li><a href="./about.html" onmouseover="alteraCorMenu(this)" onmouseout="retornaCorMenu(this)" id="teste">Sobre nós</a></li>
-                    <li><a href="./cardápio.html" onmouseover="alteraCorMenu(this)" onmouseout="retornaCorMenu(this)" id="teste">Nosso Cardápio</a></li>
+                    <li><a href="./cardápio.php" onmouseover="alteraCorMenu(this)" onmouseout="retornaCorMenu(this)" id="teste">Nosso Cardápio</a></li>
                     <li><a href="./reservas.html" onmouseover="alteraCorMenu(this)" onmouseout="retornaCorMenu(this)" id="teste">Reservas</a></li>
                     <li><a href="./atrações.html" onmouseover="alteraCorMenu(this)" onmouseout="retornaCorMenu(this)" id="teste">Atrações</a></li>
                     <li><a href="./locais.html" onmouseover="alteraCorMenu(this)" onmouseout="retornaCorMenu(this)" id="teste">Instalações Físicas</a></li>
@@ -57,9 +57,9 @@
                     "<img src='data:image/jpeg;base64,".$data[$i]['foto_produto']."'>".
                     "</div>".
                     "<div class='name-price'>".
-                    "<h2>".$data[$i]['nome_produto']."    ".$data[$i]['preco_produto']." R$"."</h2>".
+                    "<h2>".$data[$i]['nome_produto']."  --  ".$data[$i]['preco_produto']." R$"."</h2>".
                     "</div>".
-                    "<h2>".$data[$i]['nome_produto']."</h2>".
+                    "<h3>".$data[$i]['nome_produto']."</h3>".
 
                     "</div>";
                 }
